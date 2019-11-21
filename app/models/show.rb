3d5b5700d707b9e465f4b9f0fcfbeeaabb1e8ff3
Cody Rickman <cody@creativemindstudios.com>
2019-11-21 12:16:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,12 @@
 class Show < ActiveRecord::Base
   has_one :network
   has_many :characters
+  has_many :actors, through: :characters
+  def actors_list
+    list = []
+    self.actors.each do |actor|
+      list << actor
+    end
+    return list
+  end
 end
